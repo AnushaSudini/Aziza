e7e1a8205258cd258a45972187745582a7edd8b4
AnushaSudini <asudini@memphis.edu>
2015-11-01 19:57:01 +0000

    Seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,93 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#Student Names
+user = User.create(:name => "Anusha", :role => "teacher", :email => "asudini@memphis.edu" , :password => "hellohello")
+user = User.create(:name => "Maria", :role => "teacher", :email => "mluong@memphis.edu" , :password => "hellohello")  
+user = User.create(:name => "Student", :role => "student", :email => "student@memphis.edu" , :password => "software") 
+user = User.create(:name => "T", :role => "student", :email => "tsdhlwal@memphis.edu" , :password => "software") 
+user = User.create(:name => "Aaron", :role => "student", :email => "tmrshall@memphis.edu" , :password => "software") 
+user = User.create(:name => "Austin", :role => "student", :email => "prynlds3@memphis.edu" , :password => "software") 
+user = User.create(:name => "Brandon", :role => "student", :email => "bhopper1@memphis.edu" , :password => "software") 
+user = User.create(:name => "Cameron", :role => "student", :email => "cwood2@memphis.edu" , :password => "software") 
+user = User.create(:name => "Cannon", :role => "student", :email => "wcmoyer@memphis.edu" , :password => "software") 
+user = User.create(:name => "Chris", :role => "student", :email => "cdhall3@memphis.edu" , :password => "software") 
+user = User.create(:name => "Cletus", :role => "student", :email => "chhatten@memphis.edu" , :password => "software") 
+user = User.create(:name => "Cooper", :role => "student", :email => "cbjones5@memphis.edu" , :password => "software") 
+user = User.create(:name => "Daniel", :role => "student", :email => "wdwood@memphis.edu" , :password => "software") 
+user = User.create(:name => "David", :role => "student", :email => "depucktt@memphis.edu" , :password => "software") 
+user = User.create(:name => "Denzel", :role => "student", :email => "djyoung1@memphis.edu" , :password => "software") 
+user = User.create(:name => "Drew", :role => "student", :email => "dtstbnow@memphis.edu" , :password => "software") 
+user = User.create(:name => "Ed", :role => "student", :email => "prinsiii@memphis.edu" , :password => "software") 
+user = User.create(:name => "Enyil", :role => "student", :email => "pdllvlle@memphis.edu" , :password => "software") 
+user = User.create(:name => "Eric", :role => "student", :email => "egoddard@memphis.edu" , :password => "software") 
+user = User.create(:name => "Gregory", :role => "student", :email => "gllwrnce@memphis.edu" , :password => "software") 
+user = User.create(:name => "Herve", :role => "student", :email => "haniglo@memphis.edu" , :password => "software") 
+user = User.create(:name => "Jeremy", :role => "student", :email => "jqthomas@memphis.edu" , :password => "software") 
+user = User.create(:name => "Jonathan", :role => "student", :email => "jwood5@memphis.edu" , :password => "software") 
+user = User.create(:name => "Joseph", :role => "student",:email => "jmcskwsk@memphis.edu" , :password => "software")
+user = User.create(:name => "Joshua", :role => "student",:email => "jrstccio@memphis.edu" , :password => "software")
+user = User.create(:name => "Justin", :role => "student",:email => "jchiller@memphis.edu" , :password => "software")
+user = User.create(:name => "Kevin T.", :role => "student",:email => "ktwnsnd5@memphis.edu" , :password => "software")
+user = User.create(:name => "Kevin W.", :role => "student",:email => "kcwllms2@memphis.edu" , :password => "software")
+user = User.create(:name => "Khadeidre", :role => "student",:email => "kndean@memphis.edu" , :password => "software")
+user = User.create(:name => "Landon", :role => "student",:email => "lvnfrank@memphis.edu" , :password => "software")
+user = User.create(:name => "Matt", :role => "student",:email => "mcmaddox@memphis.edu" , :password => "software")
+user = User.create(:name => "Michael V.", :role => "student",:email => "mvance1@memphis.edu" , :password => "software")
+user = User.create(:name => "Miker", :role => "student",:email => "mcirick@memphis.edu" , :password => "software")
+user = User.create(:name => "Moose", :role => "student",:email => "cmuse@memphis.edu" , :password => "software")
+user = User.create(:name => "Morgan", :role => "student",:email => "masteffy@memphis.edu" , :password => "software")
+user = User.create(:name => "Nick", :role => "student",:email => "nmgordon@memphis.edu" , :password => "software")
+user = User.create(:name => "Paul", :role => "student",:email => "phdwards@memphis.edu" , :password => "software")
+user = User.create(:name => "Samuel", :role => "student",:email => "swalton2@memphis.edu" , :password => "software")
+user = User.create(:name => "Tam", :role => "student",:email => "tyduong@memphis.edu" , :password => "software")
+user = User.create(:name => "Ted", :role => "student",:email => "mprsons2@memphis.edu" , :password => "software")
+user = User.create(:name => "Tyrin", :role => "student",:email => "tgandrsn@memphis.edu" , :password => "software")
+user = User.create(:name => "Warner", :role => "student",:email => "wwjhnsn2@memphis.edu" , :password => "software")
+user = User.create(:name => "Zack", :role => "student",:email => "zjslerno@memphis.edu" , :password => "software")
+
+#Anusha's Questions
+test = Test.create(:test_name => "Prelecture Quiz Class 1", :status => "deactivated" )
+test = Test.create(:test_name => "Strategy Quiz 1", :status => "deactivated" )   
+test = Test.create(:test_name => "Strategy Quiz 2", :status => "deactivated" )   
+test = Test.create(:test_name => "Strategy Quiz 3", :status => "deactivated" )   
+#test = Test.create(:test_name => "Observer Quiz", :status => "deactivated" )
+test = Test.create(:test_name => "End of class 1 Quiz", :status => "deactivated" )
+
+test = Test.create(:test_name => "Prelecture Quiz Class 2", :status => "deactivated" )
+test = Test.create(:test_name => "Adapter Quiz 1", :status => "deactivated" )
+test = Test.create(:test_name => "Adapter Quiz 2", :status => "deactivated" )
+test = Test.create(:test_name => "Facade Quiz 1", :status => "deactivated" )
+test = Test.create(:test_name => "Facade Quiz 2", :status => "deactivated" )
+test = Test.create(:test_name => "End of class 2 Quiz", :status => "deactivated" )
+
 
 =begin
-def seed_teachers
-  @user_teacher = User.find_by_role("teacher")
-  unless @user_teacher.present?
-    User.create(:name => "Teacher", :role => "teacher", :email => "example_teacher@gmail.com" , :password => "hellohello")
-  end
-end
+mcq = MultipleChoiceQuestion.create(:test_id => "1", :description => "How many instances of an abstract class can be created?", :option_1 => "1", :option_2 => "5", :option_3 => "13", :option_4 => "0", :answer => "0" )
+mcq = MultipleChoiceQuestion.create(:test_id => "1", :description => "The process by which one object can acquire the properties of another object", :option_1 => "Inheritance", :option_2 => "Encapsulation", :option_3 => "Polymorphism", :option_4 => "Data hiding", :answer => "Inheritance" )
+mcq = MultipleChoiceQuestion.create(:test_id => "1", :description => "Here is a method definition:
+op(param1: Type1, param2: Type2): Return_Type1
+Which of the following has a different signature?
+", :option_1 => "op(param1: Type1, param2: Type2) : Return_Type1", :option_2 => "op(param1: Type1, param2: Type2) : Return_Type2
+", :option_3 => "op(param1: Type1, param2: Type2) : Return_Type2", :option_4 => "op(param1: Type1, param2: Type1) : Return_Type1", :answer => "op(param1: Type1, param2: Type1) : Return_Type1" )
 
-seed_teachers
+dsq = DescriptiveQuestion.create(:test_id => "2", :description => "Adding fly() to Duck class is a good idea? (True/False) If false, explain the reason 
+", :answer => "Not all Duck subclasses should fly")
 
+mcq = MultipleChoiceQuestion.create(:test_id => "3", :description => "Which of the following are applicable by using Flyable interface?", :option_1 => "All duck subclasses must implement Flyable interface", :option_2 => "Ducks can't quack and fly at the same time
+", :option_3 => "No Code Reusability ", :option_4 => "None", :answer => "No Code Reusability" )
+
+dsq = DescriptiveQuestion.create(:test_id => "4", :description => "What would you do if you were Joe?", :answer => "")
+
+#dsq = DescriptiveQuestion.create(:test_id => "5", :description => "What is the best way to support new types of subscribers?", :answer => "")
+
+mcq = MultipleChoiceQuestion.create(:test_id => "5", :description => "What type of relationship does the subject to observer follow?", :option_1 => "One to One", :option_2 => "One to Many", :option_3 => "Many to One", :option_4 => "Many to Many", :answer => "One to Many" )
+mcq = MultipleChoiceQuestion.create(:test_id => "5", :description => "Which of the following is considered as a force to select a Strategy Pattern? ", :option_1 => "A client needs to use a family of related objects", :option_2 => "A change to an object requires changing other objects", :option_3 => "Multiple classes are the same but differ only in their behaviours", :option_4 => "none", :answer => "Multiple classes are the same but differ only in their behaviours" )
+mcq = MultipleChoiceQuestion.create(:test_id => "5", :description => "Your program generates various outputs. You need a way to notify Facebook users when certain outputs are generated.", :option_1 => "Facade", :option_2 => "Abstract Factory", :option_3 => "Observer", :option_4 => "Factory Method", :answer => "Observer")
+
+mcq = MultipleChoiceQuestion.create(:test_id => "6", :description => "A sequence of requests and collaborations between objects in a pattern is explained by its", :option_1 => "Structure", :option_2 => "activity flow", :option_3 => "organization", :option_4 => "Interaction diagrams", :answer => "Interaction diagrams")
+mcq = MultipleChoiceQuestion.create(:test_id => "6", :description => "The classes or objects involved in the design pattern are called ", :option_1 => "collaborations", :option_2 => "participants", :option_3 => "elements", :option_4 => "features", :answer => "participants" )
+mcq = MultipleChoiceQuestion.create(:test_id => "6", :description => "You display weather forecast in various ways. Whenever weather fluctuates, all the departments are to be notified. What pattern do you use?", :option_1 => "Singleton", :option_2 => "Observer", :option_3 => "Strategy", :option_4 => "Decorator", :answer => "Observer" )
 =end
 
- user = User.create(:name => "Teacher", :role => "teacher", :email => "teacher@example.com" , :password => "hellohello"
-                  )  
-user = User.create(:name => "Student", :role => "student", :email => "student@example.com" , :password => "hellohello"
-                   )                    
-  #user.skip_confirmation!
-  #user.save
-  
+
+
+
+#Maria's Questions
