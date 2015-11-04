2d2584357340488015b17ce5246abb76103f01d5
AnushaSudini <asudini@memphis.edu>
2015-11-04 17:00:49 +0000

    report

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,10 +52,11 @@ test = Test.create(:test_name => "Activity 5", :status => "deactivated" )
 test = Test.create(:test_name => "End of class 1 Quiz", :status => "deactivated" )
 
 test = Test.create(:test_name => "Prelecture Quiz Class 2", :status => "deactivated" )
-test = Test.create(:test_name => "Adapter Quiz 1", :status => "deactivated" )
-test = Test.create(:test_name => "Adapter Quiz 2", :status => "deactivated" )
-test = Test.create(:test_name => "Facade Quiz 1", :status => "deactivated" )
-test = Test.create(:test_name => "Facade Quiz 2", :status => "deactivated" )
+test = Test.create(:test_name => "Activity 6", :status => "deactivated" )
+test = Test.create(:test_name => "Activity 7", :status => "deactivated" )
+test = Test.create(:test_name => "Activity 8", :status => "deactivated" )
+test = Test.create(:test_name => "Activity 9", :status => "deactivated" )
+test = Test.create(:test_name => "Activity 10", :status => "deactivated" )
 test = Test.create(:test_name => "End of class 2 Quiz", :status => "deactivated" )
 
 
